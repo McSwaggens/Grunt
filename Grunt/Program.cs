95f2377bb102e1043857bdd6cbc5213cf5d48ee4
Daniel Jones <xXDJONESXx@hotmail.com>
2016-04-20 20:49:44 +1000

    Initial Commit

---
+++ Grunt/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace Grunt
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Random random = new Random();
+			Console.Clear();
+			while (true) {
+				string[] files = Directory.GetFiles("/etc/");
+				foreach (string file in files)
+				{
+					int y = 0;
+					
+					string[] text = File.ReadAllLines(file);
+					
+					for (int line = 0; line < text.Length; line++)
+					{
+						Console.SetCursorPosition(0, y);
+						Console.WriteLine(text[line]);
+						Thread.Sleep(random.Next(8, 50));
+						y++;
+						if (y >= Console.BufferHeight-1) {
+							y = 0;
+							Console.Clear();
+						}
+					}
+					Console.Clear();
+				}
+			}
+		}
+	}
+}
